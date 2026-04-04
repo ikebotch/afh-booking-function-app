bd3bfba26d0cf50b50be30ef3d161b1583e5f9f8
ikebotch <ikebotch@gmail.com>
2026-04-04 08:18:29 +0100

    refactor: finish booking modularisation

---
+++ src/AFH.Booking.Application/Bookings/BookingFlowModuleServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+using AFH.Booking.Application.Abstractions.Bookings;
+using AFH.Booking.Application.Abstractions.Bookings.Handlers;
+using AFH.Booking.Application.Abstractions.Governance;
+using AFH.Booking.Application.Abstractions.Lifecycle;
+using AFH.Booking.Application.Bookings;
+using AFH.Booking.Application.Governance;
+using AFH.Booking.Application.Lifecycle;
+
+namespace AFH.Booking.Application.Composition;
+
+internal static class BookingFlowModuleServiceCollectionExtensions
+{
+    internal static IServiceCollection AddBookingFlowModule(this IServiceCollection services)
+    {
+        services.AddScoped<ICancelBookingHandler, CancelBookingHandler>();
+        services.AddScoped<IBookingDetailsHandler, BookingDetailsHandler>();
+        services.AddScoped<IRearrangementOptionsHandler, RearrangementOptionsHandler>();
+        services.AddScoped<IRearrangeBookingHandler, RearrangeBookingHandler>();
+        services.AddScoped<ICancellationOrchestrator, CancellationOrchestrator>();
+        services.AddScoped<IRearrangementOrchestrator, RearrangementOrchestrator>();
+        services.AddScoped<IBookingConflictService, BookingConflictService>();
+        services.AddScoped<ILifecycleAuditService, LifecycleAuditService>();
+
+        return services;
+    }
+}
--- src/AFH.Booking.Application/Composition/ServiceCollectionExtensions.cs
+++ src/AFH.Booking.Application/Composition/ServiceCollectionExtensions.cs
@@ -1,11 +1,5 @@
 ﻿using AFH.Booking.Application.Abstractions;
-using AFH.Booking.Application.Abstractions.Bookings;
 using AFH.Booking.Application.Abstractions.Bookings.Handlers;
-using AFH.Booking.Application.Abstractions.Governance;
-using AFH.Booking.Application.Abstractions.Lifecycle;
-using AFH.Booking.Application.Bookings;
-using AFH.Booking.Application.Governance;
-using AFH.Booking.Application.Lifecycle;
 using AFH.Booking.Application.Common;
 using AFH.Booking.Application.Common.Clock;
 
@@ -19,15 +13,7 @@ public static class ServiceCollectionExtensions
         services.AddSingleton<IClock, SystemClock>();
         services.AddSingleton<ITimeZoneProvider, DefaultTimeZoneProvider>();
 
-        // Booking handlers
-        services.AddScoped<ICancelBookingHandler, CancelBookingHandler>();
-        services.AddScoped<IBookingDetailsHandler, BookingDetailsHandler>();
-        services.AddScoped<IRearrangementOptionsHandler, RearrangementOptionsHandler>();
-        services.AddScoped<IRearrangeBookingHandler, RearrangeBookingHandler>();
-        services.AddScoped<ICancellationOrchestrator, CancellationOrchestrator>();
-        services.AddScoped<IRearrangementOrchestrator, RearrangementOrchestrator>();
-        services.AddScoped<IBookingConflictService, BookingConflictService>();
-        services.AddScoped<ILifecycleAuditService, LifecycleAuditService>();
+        services.AddBookingFlowModule();
         services.AddBookingHoldsModule();
         services.AddBookingAvailabilityModule();
 

7c9a62f902aa4832e5c1d289231f26cc7039f541
ikebotch <ikebotch@gmail.com>
2026-08-20 17:19:04 +0100

    feat(migrations): drop legacy booking notification dispatch tables and update migration logic

---
+++ src/AFH.Booking.Infrastructure/Migrations/20260820161730_DropLegacyBookingNotificationDispatchTables.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AFH.Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropLegacyBookingNotificationDispatchTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                IF OBJECT_ID(N'[dbo].[NotificationMessageLogs]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[NotificationMessageLogs];
+
+                IF OBJECT_ID(N'[dbo].[EmailBounceEvents]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[EmailBounceEvents];
+
+                IF OBJECT_ID(N'[dbo].[NotificationDispatches]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[NotificationDispatches];
+
+                IF OBJECT_ID(N'[dbo].[NotificationOutbox]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[NotificationOutbox];
+
+                IF OBJECT_ID(N'[dbo].[NotificationTemplates]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[NotificationTemplates];
+
+                IF OBJECT_ID(N'[dbo].[NotificationSettings]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[NotificationSettings];
+
+                IF OBJECT_ID(N'[dbo].[BookingNotificationRuleRecipients]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[BookingNotificationRuleRecipients];
+
+                IF OBJECT_ID(N'[dbo].[BookingNotificationRuleChannels]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[BookingNotificationRuleChannels];
+
+                IF OBJECT_ID(N'[dbo].[BookingNotificationRules]', N'U') IS NOT NULL
+                    DROP TABLE [dbo].[BookingNotificationRules];
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Intentionally no-op: notification runtime and policy data now belongs to NotificationDb.
+            // Roll forward by ensuring NotificationDb migrations are applied rather than recreating legacy BookingDb tables.
+        }
+    }
+}
